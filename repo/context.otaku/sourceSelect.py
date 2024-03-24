9105ea59efa0421328829a416a1709e9969f0fc4
Joseph Zito <60677138+joezito129@users.noreply.github.com>
2024-03-24 17:40:34 -0500

    a

---
+++ repo/context.otaku/sourceSelect.py
@@ -0,0 +1,13 @@
+import sys
+import xbmc
+
+if __name__ == '__main__':
+    item = sys.listitem
+    message = item.getLabel()
+    path = item.getPath()
+
+    matches = ['play/', 'play_movie/', 'watchlist_to_movie/']
+
+    if any(x in path for x in matches):
+        path += '?source_select=true'
+    xbmc.executebuiltin('PlayMedia(%s)' % path)
